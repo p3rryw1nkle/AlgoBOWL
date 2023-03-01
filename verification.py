cbe3069791d73ec87ea354ba1e486fe6740347cd
landnn <landonnbrownn@gmail.com>
2023-03-01 14:04:41 -0700

    Added group inputs, working version of verifier

--- verification.py
+++ verification.py
@@ -1,30 +1,29 @@
-# from collections import defaultdict
 import sys, os
 
 class Verifier():
-    def __init__(self, filename):
-        self.sequence = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-        # self.sequence = [2,5]
-        # self.sequence = self.readInput(filename)
-        self.sums = self.readOutput(filename)
-        self.result = self.checkSums()
+    def __init__(self, inputFile, outputFile):
+        # self.sequence = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+        self.sequence = self.readInput(inputFile) # reads in a sequence from a group's input file
+        self.sums = self.readOutput(outputFile) # reads in the series of summations from a group's output file
+        self.errorLog = [] # if an output is invalid, log why
+        self.result = self.checkSums() # result of the verification
 
     def readInput(self, filename) -> list:
         file = open(filename, 'r')
-        sumCount = int(file.readline()) # read in how many nodes there are from first line
+        sumCount = int(file.readline())
         nums = []
 
         line = file.readline()
         for number in line.split():
-            nums.append(number)
+            nums.append(int(number))
 
         file.close()
         return nums
 
     def readOutput(self, filename) -> list:
         file = open(filename, 'r')
-        sumCount = int(file.readline()) # read in how many nodes there are from first line
-        sums = {1}
+        sumCount = int(file.readline()) 
+        sums = {1} # keep track of current numbers that have been calculated, 1 is there by default
 
         for operation in range(sumCount):
             line = file.readline()
@@ -33,33 +32,37 @@ class Verifier():
             summation = line.split(' ')
             num1 = int(summation[0])
             num2 = int(summation[1])
-            if (num1 in sums) and (num2 in sums):
+            if (num1 in sums) and (num2 in sums): # search through set to make sure numbers have already been calculated
                 sums.add(int(summation[0]) + int(summation[1]))
             else:
-                print(f"ERROR: one of these numbers: {num1} or {num2}, has not yet been calculated!")
+                self.errorLog.append(f"ERROR: one of these numbers: {num1} or {num2}, has not yet been calculated!")
                 break
   
         file.close()
-        print(f"Calculated sums: {sums}")
         return sums
 
-    def checkSums(self) -> bool:
-        for summation in self.sums:
+    def checkSums(self) -> bool: # ASSUMES THAT SUMMATIONS ARE CALCULATED IN-ORDER (COULD BE PROBLEMATIC)
+        for summation in self.sums: # go through each sum that was calculated 
             if summation == self.sequence[0]:
                 self.sequence.pop(0)
                 continue
             if summation >= self.sequence[0]:
-                print(f"ERROR: {summation} was caclulated before {self.sequence[0]}")
+                self.errorLog.append(f"ERROR: {summation} was caclulated before {self.sequence[0]}")
                 return False
         if len(self.sequence) == 0: # if all the numbers in the sequence have been found
             return True
         else:
-            print(f"ERROR: {self.sequence} were not calculated!")
+            self.errorLog.append(f"ERROR: {self.sequence} were not calculated!")
             return False
 
-
-import os
 for filename in os.listdir('./outputs'):
-    V = Verifier(f"./outputs/{filename}")
-    print(V.result)
-
+    groupName = filename.replace("output_","")
+    V = Verifier(f"./inputs/input_{groupName}.txt", f"./outputs/{filename}")
+    # print(V.result)
+    f = open(f"./verification_results/results_{groupName}.txt", "w")
+    f.write(str(V.result) + "\n")
+    for error in V.errorLog:
+        f.write(error)
+    f.write(f"These were the summmations we calculated for {groupName}: {V.sums} \n")
+    if V.result == False:
+        f.write(f"These were the sums we were missing: {V.sequence}")
